f724e1b3a708e3644e06d860e6562283401ffc11
xiaoC <i@xiaoc.cn>
2019-08-22 18:28:45 +0800

    直接使用bson导致pymongo出现问题

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages            #这个包没有的可以
 
 setup(
     name = "cPython",      #这里是pip项目发布的名称
-    version = "0.0.1",  #版本号，数值大的会优先被pip
+    version = "0.0.3",  #版本号，数值大的会优先被pip
     keywords = ("cPython"),
     description = "more common def",
     long_description = "more common def",
@@ -25,5 +25,5 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     platforms = "any",
-    install_requires = ["bson", "requests"]          #这个项目需要的第三方库
+    install_requires = ["pymongo", "requests"]          #这个项目需要的第三方库
 )
