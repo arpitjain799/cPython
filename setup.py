68060f20fcd16393da099a377e35e4e634caefb2
xiaoC <i@xiaoc.cn>
2019-08-30 16:49:35 +0800

    沙雕了, 为什么要用def作为文件名..

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages            #这个包没有的可以
 
 setup(
     name = "cPython",      #这里是pip项目发布的名称
-    version = "0.0.3",  #版本号，数值大的会优先被pip
+    version = "0.0.4",  #版本号，数值大的会优先被pip
     keywords = ("cPython"),
     description = "more common def",
     long_description = "more common def",
